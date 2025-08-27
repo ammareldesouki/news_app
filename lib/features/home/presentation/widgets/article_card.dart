b9ac8f1200cb8b0cada2084c179a7ffc562b5eda
Ammar Mohamed <132349140+ammareldesouki@users.noreply.github.com>
2025-08-27 21:08:54 +0300

    update the container

--- lib/features/home/presentation/widgets/article_card.dart
+++ lib/features/home/presentation/widgets/article_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/models/article_model.dart';
+import 'package:news_app/core/services/app_setting_provider.dart';
+import 'package:provider/provider.dart';
 
 class ArticleCard extends StatelessWidget{
   final ArticleDetailesModel articleDetailesModel;
@@ -7,13 +9,16 @@ class ArticleCard extends StatelessWidget{
   const ArticleCard({super.key, required this.articleDetailesModel});
   @override
   Widget build(BuildContext context) {
+    bool dark = Provider
+        .of<AppSettingProvider>(context)
+        .isDarkMode;
 
     return  Container(
       padding: EdgeInsets.all(10),
     height:  322,
      width: double.infinity,
      decoration: BoxDecoration(
-       border: Border.all(color: Colors.black),
+         border: Border.all(color: dark ? Colors.white : Colors.black),
        borderRadius: BorderRadius.circular( 16)
      ),
      child: Column(
